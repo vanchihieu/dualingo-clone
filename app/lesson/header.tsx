b15c9dd0b3072ce5b44a7fe3f39485cb34915f2c
Van Chi Hieu <vanchihieu3@gmail.com>
2024-05-01 10:42:28 +0700

    Exit Modal

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -2,7 +2,7 @@ import { InfinityIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { Progress } from "@/components/ui/progress";
-// import { useExitModal } from "@/store/use-exit-modal";
+import { useExitModal } from "@/store/use-exit-modal";
 
 type HeaderProps = {
   hearts: number;
@@ -15,12 +15,12 @@ export const Header = ({
   percentage,
   hasActiveSubscription,
 }: HeaderProps) => {
-  // const { open } = useExitModal();
+  const { open } = useExitModal();
 
   return (
     <header className="mx-auto flex w-full max-w-[1140px] items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
       <X
-        onClick={() => {}}
+        onClick={open}
         className="cursor-pointer text-slate-500 transition hover:opacity-75"
       />
 
